f724c37f236fc2673be4e3f98b29860968a64cd2
angus <angus@eventx.io>
2025-04-01 12:26:43 +0800

    refractor

---
+++ script.js
@@ -0,0 +1,21 @@
+if (window.location.href.indexOf("general") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("priority") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=11f9f42a-6cee-4101-977f-d6b51e7e4a1a")
+} else if (window.location.href.indexOf("staff") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("guest") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("exhibitor") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("group") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("media") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("ex-overseas") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+} else if (window.location.href.indexOf("overseas") > -1) {
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+}  else {
+  window.location.replace("https://rethink-event.com/")
+}
