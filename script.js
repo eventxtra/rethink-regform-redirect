4efd569856dcea82d68d3fd7fe9972ec56cee85d
angus <angus@eventx.io>
2025-04-25 10:38:20 +0800

    group form redirect

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (window.location.href.indexOf("general") > -1) {
 } else if (window.location.href.indexOf("exhibitor") > -1) {
     window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
 } else if (window.location.href.indexOf("group") > -1) {
-    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+    window.location.replace("https://eventxapp.com/events/39ecd2b8-6a63-445e-afc8-b9129ab99765?regForm=421c5d4d-1427-4175-8aca-3dab4fc3f474")
 } else if (window.location.href.indexOf("media") > -1) {
     window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
 } else if (window.location.href.indexOf("ex-overseas") > -1) {
