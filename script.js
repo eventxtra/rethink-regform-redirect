58c938cdbb44bd7e93cc092bce0db60a6340271c
angus <angus@eventx.io>
2025-05-02 16:04:41 +0800

    update the URL

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ if (window.location.href.indexOf("general") > -1) {
 } else if (window.location.href.indexOf("priority") > -1) {
     window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
 } else if (window.location.href.indexOf("staff") > -1) {
-    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=129ad06e-2ccb-44de-90ca-4870444a2b59")
 } else if (window.location.href.indexOf("guest") > -1) {
-    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=6c5aa6ae-2478-423a-8d40-be55b8ffd6d8")
 } else if (window.location.href.indexOf("exhibitor") > -1) {
-    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=d9fe267d-a45a-42f2-ab34-d3d12ac8fa30")
 } else if (window.location.href.indexOf("group") > -1) {
     window.location.replace("https://eventxapp.com/events/39ecd2b8-6a63-445e-afc8-b9129ab99765?regForm=421c5d4d-1427-4175-8aca-3dab4fc3f474")
 } else if (window.location.href.indexOf("media") > -1) {
-    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=386b4eb0-a0bb-4a7f-8a88-be36ad96cfca")
 } else if (window.location.href.indexOf("ex-overseas") > -1) {
     window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
 } else if (window.location.href.indexOf("overseas") > -1) {
-    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=78181eac-3a2a-4fc9-bf56-f363e496012d")
+    window.location.replace("https://eventxapp.com/events/9d10d1c6-75d8-4996-a053-7fb231d3eac1?regForm=9634b90c-0517-48c5-8d67-9ad46a7675de")
 }  else {
   window.location.replace("https://rethink-event.com/")
 }
